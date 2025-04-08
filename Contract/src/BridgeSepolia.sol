e1cf5f8d7a4c45f243bfefbdfeeda064246ce9fd
JamesVictor-o <victorjames40v@gmail.com>
2025-04-08 15:05:50 +0100

    ...

--- Contract/src/BridgeSepolia.sol
+++ Contract/src/BridgeSepolia.sol
@@ -7,17 +7,35 @@ import "./library/Errors.sol";
 import "./library/Events.sol";
 import "./tokens/Bbl.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "./interfaces/IUniswapV2Pair.sol";   
 
 contract BridgeSepolia is BridgeBaseAbstract, ReentrancyGuard {
-    Bbl public token;
+    Fabs public token;
+    IUniswapV2Pair public ammPool;
+    address public stablecoin; // 
+ 
+    constructor(address _token, address _ammPool, address _stablecoin) {
+        token = Fabs(_token);
+        ammPool = IUniswapV2Pair(_ammPool);
+        stablecoin = _stablecoin;
+    }
 
-    constructor(address _token) {
-        token = Bbl(_token);
+    // uint256 destinationAmount = (sourceAmount * 250) / 100; 
+
+    function getTokenPrice() public view returns (uint256) {
+        (uint112 reserve0, uint112 reserve1,) = ammPool.getReserves();
+        uint256 tokenPrice = (uint256(reserve0) * 1e18) / uint256(reserve1);
+        return tokenPrice;
+    } 
+    function getDestinationAmount(uint256 sourceAmount) public view returns (uint256) {
+        uint256 tokenPrice = getTokenPrice();
+        uint256 destinationAmount = (sourceAmount * tokenPrice) / 1e18;
+        return destinationAmount;
     }
 
     function lockTokens(uint256 amount, string memory targetChain) external nonReentrant {
         token.transferFrom(msg.sender, address(this), amount);
-        emit Event.BridgeInitiated(msg.sender, amount, targetChain, keccak256(abi.encodePacked(msg.sender, amount, block.timestamp)));
+        emit Event.BridgeInitiated(msg.sender, destinationAmount, targetChain, keccak256(abi.encodePacked(msg.sender, destinationAmount, block.timestamp)));
     }
 
     function releaseTokens(address to, uint256 amount, bytes32 sourceTx) external onlyOnce(sourceTx) {
---
+++ Contract/src/PoolCreator.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.8.26;
+
+import "v2-core/interfaces/IUniswapV2Factory.sol";
+import "v2-core/interfaces/IUniswapV2Pair.sol";
+
+contract PoolCreator {
+    address public immutable UNISWAP_FACTORY;
+    
+    constructor(address _factory) {
+        UNISWAP_FACTORY = _factory;
+    }
+    
+    function createPool(address tokenA, address tokenB) external returns (address poolAddress) {
+        // Sort tokens (Uniswap requirement)
+        (address token0, address token1) = tokenA < tokenB 
+            ? (tokenA, tokenB) 
+            : (tokenB, tokenA);
+        
+        poolAddress = IUniswapV2Factory(UNISWAP_FACTORY).createPair(token0, token1);
+    }
+    
+    function getPool(address tokenA, address tokenB) external view returns (address) {
+        return IUniswapV2Factory(UNISWAP_FACTORY).getPair(tokenA, tokenB);
+    }
+}
